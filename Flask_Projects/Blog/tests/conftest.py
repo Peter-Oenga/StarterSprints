9bf0af0bafb9e306c1666592eb263a365ef9758b
Peter-Oenga <poenga230@gmail.com>
2025-04-11 12:21:29 +0300

    Jinja templating engine

---
+++ Flask_Projects/Blog/tests/conftest.py
@@ -0,0 +1,55 @@
+import os
+import tempfile
+
+import pytest
+
+from flaskr import create_app
+from flaskr.db import get_db, init_db
+
+with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
+    __data_sql = f.read().decode('utf')
+
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+    })
+
+    with app.app_context():
+        init_db()
+        get_db().executescript(__data_sql)
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
+
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='test', password='test'):
+        return self.__client.post(
+            '/auth/login',
+            data={'username': username, 'password':password}
+        )
+    
+    def logout(self):
+        return self.__client.get('/auth/logout')
+    
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
