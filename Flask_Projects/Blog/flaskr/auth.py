272ee8a27abd18fc87aff684917970bf2e05a8ad
Peter-Oenga <poenga230@gmail.com>
2025-04-11 06:15:41 +0300

    Added the test  coverage

--- Flask_Projects/Blog/flaskr/auth.py
+++ Flask_Projects/Blog/flaskr/auth.py
@@ -45,8 +45,8 @@ def register():
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
-        username = request.form('username')
-        password = request.form('password')
+        username = request.form['username']
+        password = request.form['password']
         db = get_db()
         error = None
         user = db.execute(
--- Flask_Projects/Blog/flaskr/schema.sql
+++ Flask_Projects/Blog/flaskr/schema.sql
@@ -15,5 +15,6 @@ CREATE TABLE post (
     author_id INTEGER NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title TEXT NOT NULL,
+    body TEXT NOT NULL,
     FOREIGN KEY (author_id) REFERENCES user (id)
 );
