219e4bb14f34602c0b327252192007d8aa375e5f
Peter-Oenga <poenga230@gmail.com>
2025-11-26 22:30:17 +0300

    Updated the role play

--- freecodecamp/legacy_js_data_structures_v8/RolePlayGame/role_play_game.js
+++ freecodecamp/legacy_js_data_structures_v8/RolePlayGame/role_play_game.js
@@ -16,6 +16,12 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [
+  { name: 'stick', power: 5 },
+  { name: 'dagger', power: 30 },
+  { name: 'claw hammer', power: 50 },
+  { name: 'sword', power: 100 }
+];
 const locations = [
   {
     name: "town square",
@@ -69,13 +75,32 @@ function fightDragon() {
 }
 
 function buyHealth() {
-  gold -= 10;
-  health += 10;
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
 }
 
 function buyWeapon() {
-
+  if (currentWeaponIndex < weapons.length) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeaponIndex++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeaponIndex].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  }
 }
+
 
 function fightSlime() {
 
